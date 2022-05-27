49ade87c87bd386e8782a4b1e581cb8d20d2b77a
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-27 17:29:11 +0200

    add loggin and db

--- src/main/java/com/todo/todoapp/controllers/ToDoRestController.java
+++ src/main/java/com/todo/todoapp/controllers/ToDoRestController.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import com.todo.todoapp.dto.ToDoDto;
 import com.todo.todoapp.services.ToDoService;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/toDo")
 public class ToDoRestController {
 
-    // private static final Logger logger =
-    // LoggerFactory.getLogger(ToDoRestController.class);
+    private static Logger logger = LoggerFactory.getLogger(ToDoRestController.class);
 
     private final ToDoService toDoService;
 
@@ -29,31 +29,37 @@ public class ToDoRestController {
 
     @PostMapping
     public ToDoDto saveToDo(@RequestBody ToDoDto toDoDto) {
+        logger.info("salvataggio task");
         return this.toDoService.saveToDo(toDoDto);
     }
 
     @GetMapping
     public List<ToDoDto> getAllToDo() {
+        logger.info("restuisce tutti i task");
         return this.toDoService.getAllToDo();
     }
 
     @GetMapping("/{id}")
     public ToDoDto getByIdToDo(@PathVariable("id") Long id) {
+        logger.info("restuisce il task per id");
         return this.toDoService.getToDoById(id);
     }
 
     @GetMapping("/searchWord/{word}")
     public List<ToDoDto> getByWordToDo(@PathVariable("word") String word) {
+        logger.info("restuisce il task per parola chiave");
         return this.toDoService.getToDoByText(word);
     }
 
     @PutMapping("/{id}")
     public ToDoDto editToDo(@PathVariable("id") Long id, @RequestBody ToDoDto toDoDto) {
+        logger.info("modifica il task selezionato per id");
         return this.toDoService.editToDo(id, toDoDto);
     }
 
     @DeleteMapping("/{id}")
     public void deleteTodo(@PathVariable("id") Long id) {
+        logger.info("modifico il task selezionato per id");
         this.toDoService.deleteToDoById(id);
     }
 }
---
+++ src/main/resources/database/todo_202205271558.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.todo ("text",date_creation,date_due,state) VALUES
+	 ('dffddf','2000-12-12','2001-12-12','TODO'),
+	 ('mangiare','2000-12-12','2001-12-12','TODO'),
+	 ('bere','2000-12-12','2001-12-12','TODO'),
+	 ('giocare a calcio','2022-05-27','2001-12-04','TODO'),
+	 ('studiare','2022-05-27','2022-06-03','TODO'),
+	 ('cantare','2022-05-27','2022-06-03','TODO'),
+	 ('giro in canoa','2022-05-27','2022-06-09','TODO'),
+	 ('mare','2022-05-27','2022-06-09','DONE'),
+	 ('dffddf','2000-12-12','2001-12-12','TODO');
